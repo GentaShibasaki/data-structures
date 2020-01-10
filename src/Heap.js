fe42b4c08f50ede7124dfd285b5695ed78b172e4
travisricks <ricks.travis@gmail.com>
2020-01-10 17:03:28 +0900

    started Heap

--- src/Heap.js
+++ src/Heap.js
@@ -1,1 +1,25 @@
-class Heap {}
+class Heap {
+  constructor() {
+    this.storage = [];
+  }
+
+  insert(value) {
+    this.storage.push(value);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let index = this.storage.length - 1;
+    while (index > 0) {
+      let tmp = this.storage[index];
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.storage[parentIndex];
+
+      if (parent >= tmp) break;
+      this.storage[index] = parent;
+      this.storage[parentIndex] = tmp;
+      index = parentIndex;
+    }
+  }
+}
+
+module.exports = Heap;
